eb413cea506452966be42d79a339187f70d7605d
Richard Jia <richardjia@Richards-MacBook-Air-5.local>
2024-01-01 23:04:23 -0800

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import waifu_green_logo from './waifu_green_logo.svg'
+import preview from './preview.png';
+
+export {
+  download,
+  logo,
+  preview,
+  waifu_green_logo,
+};
---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,48 @@
+import express from "express";
+import * as dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
+const router = express.Router();
+
+
+router.route('/').get((req,res) => {
+    res.send('Dalle api test');
+});
+
+router.route('/').post(async (req, res) => {
+    try {
+        const {prompt} = req.body;
+        const inputData = {
+            inputs: prompt,
+            options: {
+                wait_for_model: true
+            }
+        };
+
+        const response = await axios({
+            url: "https://api-inference.huggingface.co/models/hakurei/waifu-diffusion",
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${process.env.HF_API_KEY}`,
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            data: JSON.stringify(inputData),
+            responseType: 'arraybuffer',
+        })
+
+        const mimeType = response.headers['content-type'];
+        const result = response.data;
+        const base64data = Buffer.from(result).toString('base64');
+        const image =  `data:${mimeType};base64,` + base64data;
+
+        res.status(200).json({photo:image});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message)
+    }
+})
+
+export default router;
